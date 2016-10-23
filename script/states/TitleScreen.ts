d7e0b0c1141055728b07c68fafae4069fe5dfdfd
agoemans <kewlboontjies@gmail.com>
2016-10-23 18:43:04 +0200

    added art credit

--- script/states/TitleScreen.ts
+++ script/states/TitleScreen.ts
@@ -7,26 +7,43 @@ module AdventureRoo {
         public init():void {
             this.game.stage.backgroundColor = '#ffffff';
 
-            let levelEndTextStyle: any = { font: "45px Arial", fill: "#547980", align: "center" };
-            let levelEndTextValue: string = 'A fun little adventure game! \n' +
+            let levelEndTextStyle:any = {font: "45px Arial", fill: "#547980", align: "center"};
+            let levelEndTextValue:string = 'A fun little adventure game! \n' +
                 'Collect the drinks, get to the exit!';
-            let levelEndText: Phaser.Text = new Phaser.Text(this.game, this.game.width / 2, this.game.height / 3, levelEndTextValue, levelEndTextStyle);
+            let levelEndText:Phaser.Text = new Phaser.Text(this.game, this.game.width / 2, this.game.height / 5, levelEndTextValue, levelEndTextStyle);
             levelEndText.anchor.set(0.5);
             this.game.add.existing(levelEndText);
 
-            let character: Phaser.Sprite = new Phaser.Sprite(this.game, this.game.width / 3 + 50, this.game.height / 2, 'MainCharacter', 0);
+            let character:Phaser.Sprite = new Phaser.Sprite(this.game, this.game.width / 3 + 50, this.game.height / 3, 'MainCharacter', 0);
             this.game.add.existing(character);
 
-            for (let i: number = 0 ; i < 2; i++){
-                let drink: Phaser.Sprite = new Phaser.Sprite(this.game, this.game.width / 2 + (i * 50) , this.game.height / 2, 'Drink', i);
+            for ( let i:number = 0; i < 2; i++ ) {
+                let drink:Phaser.Sprite = new Phaser.Sprite(this.game, this.game.width / 2 + (i * 50), this.game.height / 3, 'Drink', i);
                 this.game.add.existing(drink);
             }
 
-            let button: Phaser.Button = new Phaser.Button(this.game, this.game.width / 2, this.game.height - 100, 'StartButton', (): void => {
+            let artCreditTextStyle:any = {font: "35px Arial", fill: "#547980", align: "center"};
+            let artCreditTextValue:string = 'Art Tile set source: \nSimple Broad Purpose set';
+            let artCreditText:Phaser.Text = new Phaser.Text(this.game, this.game.width / 2, this.game.height - 200, artCreditTextValue, artCreditTextStyle);
+            artCreditText.anchor.set(0.5);
+            this.game.add.existing(artCreditText);
+
+            artCreditText.inputEnabled = true;
+            artCreditText.input.enabled = true;
+            artCreditText.input.useHandCursor = true;
+
+            artCreditText.addColor('#351330', 20);
+
+            artCreditText.events.onInputUp.add(()=> {
+               window.open('http://opengameart.org/content/simple-broad-purpose-tileset', '_blank');
+            });
+
+            let button:Phaser.Button = new Phaser.Button(this.game, this.game.width / 2, this.game.height - 100, 'StartButton', ():void => {
                 //Go to Game state
                 this.game.state.start('GamePlay');
             });
             button.anchor.set(0.5);
+            button.scale.set(0.8);
             this.game.add.existing(button);
 
         }
